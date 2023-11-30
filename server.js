8b8ee970dc65a7c9c44ed6a68bcea7ee3edef528
Thomas.Pourchel <thomas.pourchel@gmail.com>
2023-11-30 17:08:15 +0100

    route hello world fonctionnelle

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 3000
 
 app.use(express.json())
 
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
